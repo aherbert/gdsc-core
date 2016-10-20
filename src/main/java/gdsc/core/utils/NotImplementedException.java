69a10c37e0ad4ac71705b2772c7588df26afdb65
aherbert <a.herbert@sussex.ac.uk>
2016-10-20 14:33:54 +0100

    Added versions with message and throwable

--- src/main/java/gdsc/core/utils/NotImplementedException.java
+++ src/main/java/gdsc/core/utils/NotImplementedException.java
@@ -22,5 +22,21 @@ public class NotImplementedException extends RuntimeException
 
 	public NotImplementedException()
 	{
+		super();
+	}
+
+	public NotImplementedException(String message)
+	{
+		super(message);
+	}
+
+	public NotImplementedException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public NotImplementedException(Throwable cause)
+	{
+		super(cause);
 	}
 }
