013f01aaed0e547f45f760240648691e171a0430
aherbert <a.herbert@sussex.ac.uk>
2016-12-05 13:09:28 +0000

    Added licence comment

--- src/main/java/gdsc/core/ij/AlignImagesFFT.java
+++ src/main/java/gdsc/core/ij/AlignImagesFFT.java
@@ -1,5 +1,18 @@
 package gdsc.core.ij;
 
+/*----------------------------------------------------------------------------- 
+ * GDSC Plugins for ImageJ
+ * 
+ * Copyright (C) 2016 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
 import java.awt.Rectangle;
 
 import org.apache.commons.math3.util.FastMath;
--- src/main/java/gdsc/core/ij/BufferedTextWindow.java
+++ src/main/java/gdsc/core/ij/BufferedTextWindow.java
@@ -1,7 +1,7 @@
 package gdsc.core.ij;
 
 /*----------------------------------------------------------------------------- 
- * GDSC SMLM Software
+ * GDSC Plugins for ImageJ
  * 
  * Copyright (C) 2016 Alex Herbert
  * Genome Damage and Stability Centre
@@ -9,7 +9,7 @@ package gdsc.core.ij;
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *---------------------------------------------------------------------------*/
 
