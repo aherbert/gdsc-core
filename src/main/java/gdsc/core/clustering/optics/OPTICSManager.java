6308bb70aee99a72fe3b75942bcd04f0e718e674
aherbert <a.herbert@sussex.ac.uk>
2016-12-09 15:32:42 +0000

    Refactor to have a Space to store the molecules

--- src/main/java/gdsc/core/clustering/optics/OPTICSManager.java
+++ src/main/java/gdsc/core/clustering/optics/OPTICSManager.java
@@ -487,20 +487,85 @@ public class OPTICSManager extends CoordinateStore
 	/**
 	 * Used in the OPTICS algorithm
 	 */
-	private class OPTICSMoleculeGrid
+	private abstract class MoleculeSpace
 	{
-		int resolution;
 		final float generatingDistanceE;
-		final float binWidth;
-		final Molecule[][][] grid;
 		final Molecule[] setOfObjects;
-		final int xBins;
-		final int yBins;
+		final int size = xcoord.length;
+		// Working storage for find neighbours
+		final MoleculeList neighbours = new MoleculeList(size);
 
-		OPTICSMoleculeGrid(float generatingDistanceE)
+		MoleculeSpace(float generatingDistanceE)
 		{
 			this.generatingDistanceE = generatingDistanceE;
+			setOfObjects = generate();
+		}
 
+		abstract Molecule[] generate();
+
+		void reset()
+		{
+			for (int i = setOfObjects.length; i-- > 0;)
+				setOfObjects[i].reset();
+		}
+
+		/**
+		 * Find neighbours closer than the generating distance. The neighbours are written to the working memory store.
+		 * <p>
+		 * If the number of points is definitely below the minimum number of points then no distances are computed (to
+		 * save
+		 * time).
+		 * <p>
+		 * The neighbours includes the actual point in the list of neighbours (where the distance would be 0).
+		 *
+		 * @param minPts
+		 *            the min pts
+		 * @param object
+		 *            the object
+		 * @param e
+		 *            the generating distance
+		 */
+		abstract void findNeighbours(int minPts, Molecule object, float e);
+
+		/**
+		 * Find neighbours closer than the generating distance. The neighbours are written to the working memory store.
+		 * The
+		 * distances are stored in the objects encountered.
+		 * <p>
+		 * If the number of points is definitely below the minimum number of points then no distances are computed (to
+		 * save
+		 * time).
+		 * <p>
+		 * The neighbours includes the actual point in the list of neighbours (where the distance would be 0).
+		 *
+		 * @param minPts
+		 *            the min pts
+		 * @param object
+		 *            the object
+		 * @param e
+		 *            the generating distance
+		 */
+		abstract void findNeighboursAndDistances(int minPts, Molecule object, float e);
+	}
+
+	/**
+	 * Used in the OPTICS algorithm
+	 */
+	private class GridMoleculeSpace extends MoleculeSpace
+	{
+		int resolution;
+		float binWidth;
+		int xBins;
+		int yBins;
+		Molecule[][][] grid;
+
+		GridMoleculeSpace(float generatingDistanceE)
+		{
+			super(generatingDistanceE);
+		}
+
+		Molecule[] generate()
+		{
 			final float xrange = maxXCoord - minXCoord;
 			final float yrange = maxYCoord - minYCoord;
 
@@ -570,7 +635,7 @@ public class OPTICSManager extends CoordinateStore
 			yBins = 1 + (int) (yrange / binWidth);
 
 			Molecule[][] linkedListGrid = new Molecule[xBins][yBins];
-			setOfObjects = new Molecule[xcoord.length];
+			Molecule[] setOfObjects = new Molecule[xcoord.length];
 			for (int i = 0; i < xcoord.length; i++)
 			{
 				final float x = xcoord[i];
@@ -600,6 +665,8 @@ public class OPTICSManager extends CoordinateStore
 					grid[xBin][yBin] = list;
 				}
 			}
+
+			return setOfObjects;
 		}
 
 		private int determineResolution(float xrange, float yrange)
@@ -649,6 +716,169 @@ public class OPTICSManager extends CoordinateStore
 			for (int i = setOfObjects.length; i-- > 0;)
 				setOfObjects[i].reset();
 		}
+
+		/**
+		 * Find neighbours closer than the generating distance. The neighbours are written to the working memory store.
+		 * <p>
+		 * If the number of points is definitely below the minimum number of points then no distances are computed (to
+		 * save
+		 * time).
+		 * <p>
+		 * The neighbours includes the actual point in the list of neighbours (where the distance would be 0).
+		 *
+		 * @param minPts
+		 *            the min pts
+		 * @param object
+		 *            the object
+		 * @param e
+		 *            the generating distance
+		 */
+		void findNeighbours(int minPts, Molecule object, float e)
+		{
+			final int xBin = object.xBin;
+			final int yBin = object.yBin;
+
+			neighbours.clear();
+
+			// Pre-compute range
+			final int minx = Math.max(xBin - resolution, 0);
+			final int maxx = Math.min(xBin + resolution + 1, xBins);
+			final int miny = Math.max(yBin - resolution, 0);
+			final int maxy = Math.min(yBin + resolution + 1, yBins);
+
+			// Count if there are enough neighbours
+			int count = minPts;
+			counting: for (int x = minx; x < maxx; x++)
+			{
+				final Molecule[][] column = grid[x];
+				for (int y = miny; y < maxy; y++)
+				{
+					final Molecule[] list = column[y];
+					if (list != null)
+					{
+						count -= list.length;
+						if (count <= 0)
+							break counting;
+					}
+				}
+			}
+
+			if (count > 0)
+			{
+				// Not a core point so do not compute distances
+				//System.out.println("Skipping distance computation (not a core point)");
+				return;
+			}
+
+			// Compute distances
+			for (int x = minx; x < maxx; x++)
+			{
+				final Molecule[][] column = grid[x];
+				for (int y = miny; y < maxy; y++)
+				{
+					final Molecule[] list = column[y];
+					if (list != null)
+					{
+						for (int i = list.length; i-- > 0;)
+						{
+							final float d = object.distance2(list[i]);
+							if (d <= e)
+							{
+								// Build a list of all the neighbours
+								neighbours.add(list[i]);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/**
+		 * Find neighbours closer than the generating distance. The neighbours are written to the working memory store.
+		 * The
+		 * distances are stored in the objects encountered.
+		 * <p>
+		 * If the number of points is definitely below the minimum number of points then no distances are computed (to
+		 * save
+		 * time).
+		 * <p>
+		 * The neighbours includes the actual point in the list of neighbours (where the distance would be 0).
+		 *
+		 * @param minPts
+		 *            the min pts
+		 * @param object
+		 *            the object
+		 * @param e
+		 *            the generating distance
+		 */
+		void findNeighboursAndDistances(int minPts, Molecule object, float e)
+		{
+			final int xBin = object.xBin;
+			final int yBin = object.yBin;
+
+			neighbours.clear();
+
+			// Pre-compute range
+			final int minx = Math.max(xBin - resolution, 0);
+			final int maxx = Math.min(xBin + resolution + 1, xBins);
+			final int miny = Math.max(yBin - resolution, 0);
+			final int maxy = Math.min(yBin + resolution + 1, yBins);
+
+			// Count if there are enough neighbours
+			int count = minPts;
+			counting: for (int x = minx; x < maxx; x++)
+			{
+				final Molecule[][] column = grid[x];
+				for (int y = miny; y < maxy; y++)
+				{
+					final Molecule[] list = column[y];
+					if (list != null)
+					{
+						count -= list.length;
+						if (count <= 0)
+							break counting;
+					}
+				}
+			}
+
+			if (count > 0)
+			{
+				// Not a core point so do not compute distances
+				//System.out.println("Skipping distance computation (not a core point)");
+				return;
+			}
+
+			// Compute distances
+			for (int x = minx; x < maxx; x++)
+			{
+				final Molecule[][] column = grid[x];
+				for (int y = miny; y < maxy; y++)
+				{
+					final Molecule[] list = column[y];
+					if (list != null)
+					{
+						for (int i = list.length; i-- > 0;)
+						{
+							final float d = object.distance2(list[i]);
+							if (d <= e)
+							{
+								// Build a list of all the neighbours and their working distance
+								final Molecule otherObject = list[i];
+								otherObject.d = d;
+								neighbours.add(otherObject);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private class RadialMoleculeSpace
+	{
+		// TODO - Build a search space that can use a circular mask to only search over the required 
+		// points in the 2D grid
 	}
 
 	/**
@@ -788,8 +1018,7 @@ public class OPTICSManager extends CoordinateStore
 		return optics;
 	}
 
-	private OPTICSMoleculeGrid grid;
-	private MoleculeList neighbours;
+	private MoleculeSpace grid;
 	private float[] floatArray;
 
 	/**
@@ -812,16 +1041,13 @@ public class OPTICSManager extends CoordinateStore
 			if (tracker != null)
 				tracker.log("Initialising OPTICS ...");
 
-			grid = new OPTICSMoleculeGrid(generatingDistanceE);
-
-			final int size = xcoord.length;
-			neighbours = new MoleculeList(size);
+			// TODO - options to control the type of space we use to store the data
+			grid = new GridMoleculeSpace(generatingDistanceE);
 		}
 		else
 		{
 			// This is the same distance so the objects can be reused
 			grid.reset();
-			neighbours.clear();
 		}
 
 		if (floatArray == null || floatArray.length < minPts)
@@ -875,7 +1101,6 @@ public class OPTICSManager extends CoordinateStore
 	public void clearMemory()
 	{
 		grid = null;
-		neighbours = null;
 		floatArray = null;
 	}
 
@@ -897,39 +1122,9 @@ public class OPTICSManager extends CoordinateStore
 	private boolean expandClusterOrder(Molecule object, float e, int minPts, OPTICSResultList orderedFile,
 			OPTICSMoleculePriorityQueue orderSeeds)
 	{
-		// TODO: Re-write the algorithm so that each connected cluster is generated
-		// in order of reachability distance
-
-		// Note: The original paper just processes the next unprocessed object in an UNDEFINED order.
-		// However once started the remaining neighbours are processed
-		// in order of the reachability distance, which is equal to or larger
-		// than the most recent core distance. But the subsequent objects visited may
-		// have a lower core distance than the first (due to the arbitrary start point),
-		// i.e. they are in a region of higher density. This also means that: 
-		// 1. Points that have already been processed may have been reachable from a closer
-		// point (i.e. from another direction) and so should have a lower reachability 
-		// distance.
-		// 2. Points which are ignored since they have been processed may have a lower reachability
-		// distance.
-		// This means that the output reachability distance is only indicative of the walk the algorithm
-		// took through the data. It should not be used to perform actual clustering (e.g. using 
-		// the pseudocode ExtractDBSCAN-Clustering from the original paper). The authors even note 
-		// this in their paper: 
-		// "The clustering created from a cluster-ordered data set by Ex-
-		// tractDBSCAN-Clustering is nearly indistinguishable from a
-		// clustering created by DBSCAN. Only some border objects may
-		// be missed when extracted by the algorithm ExtractDBSCAN-
-		// Clustering if they were processed by the algorithm OPTICS be-
-		// fore a core object of the corresponding cluster had been found."
-
-		// To overcome this we could switch the point at which points are ignored. 
-		// The orderSeeds contains all the reachable points within the current connected cluster.
-		// We ensure that the reachable distance is updated even if the point has been processed.
-		// We just do not repeat process the neighbours of a point that has been processed.
-
-		findNeighbours(minPts, object, e);
+		grid.findNeighboursAndDistances(minPts, object, e);
 		object.markProcessed();
-		object.setCoreDistance(minPts, neighbours);
+		object.setCoreDistance(minPts, grid.neighbours);
 		if (orderedFile.add(object))
 			return true;
 
@@ -938,101 +1133,22 @@ public class OPTICSManager extends CoordinateStore
 			// Create seed-list for further expansion.
 			// The next counter is used to ensure we sort only the remaining entries in the seed list.
 			orderSeeds.clear();
-			update(orderSeeds, neighbours, object);
+			update(orderSeeds, grid.neighbours, object);
 
 			while (orderSeeds.hasNext())
 			{
 				object = orderSeeds.next();
-				findNeighbours(minPts, object, e);
+				grid.findNeighboursAndDistances(minPts, object, e);
 				object.markProcessed();
-				object.setCoreDistance(minPts, neighbours);
+				object.setCoreDistance(minPts, grid.neighbours);
 				if (orderedFile.add(object))
 					return true;
 
 				if (object.coreDistance != UNDEFINED)
-					update(orderSeeds, neighbours, object);
+					update(orderSeeds, grid.neighbours, object);
 			}
 		}
 		return false;
-	}
-
-	/**
-	 * Find neighbours closer than the generating distance. The neighbours are written to the working memory store. The
-	 * distances are stored in the objects encountered.
-	 * <p>
-	 * If the number of points is definitely below the minimum number of points then no distances are computed (to save
-	 * time).
-	 * <p>
-	 * The neighbours includes the actual point in the list of neighbours (where the distance would be 0).
-	 *
-	 * @param minPts
-	 *            the min pts
-	 * @param object
-	 *            the object
-	 * @param e
-	 *            the generating distance
-	 */
-	private void findNeighbours(int minPts, Molecule object, float e)
-	{
-		final int xBin = object.xBin;
-		final int yBin = object.yBin;
-
-		neighbours.clear();
-
-		// Pre-compute range
-		final int resolution = grid.resolution;
-		final int minx = Math.max(xBin - resolution, 0);
-		final int maxx = Math.min(xBin + resolution + 1, grid.xBins);
-		final int miny = Math.max(yBin - resolution, 0);
-		final int maxy = Math.min(yBin + resolution + 1, grid.yBins);
-
-		// Count if there are enough neighbours
-		int count = minPts;
-		counting: for (int x = minx; x < maxx; x++)
-		{
-			final Molecule[][] column = grid.grid[x];
-			for (int y = miny; y < maxy; y++)
-			{
-				final Molecule[] list = column[y];
-				if (list != null)
-				{
-					count -= list.length;
-					if (count <= 0)
-						break counting;
-				}
-			}
-		}
-
-		if (count > 0)
-		{
-			// Not a core point so do not compute distances
-			//System.out.println("Skipping distance computation (not a core point)");
-			return;
-		}
-
-		// Compute distances
-		for (int x = minx; x < maxx; x++)
-		{
-			final Molecule[][] column = grid.grid[x];
-			for (int y = miny; y < maxy; y++)
-			{
-				final Molecule[] list = column[y];
-				if (list != null)
-				{
-					for (int i = list.length; i-- > 0;)
-					{
-						final float d = object.distance2(list[i]);
-						if (d <= e)
-						{
-							// Build a list of all the neighbours and their working distance
-							final Molecule otherObject = list[i];
-							otherObject.d = d;
-							neighbours.add(otherObject);
-						}
-					}
-				}
-			}
-		}
 	}
 
 	/**
@@ -1167,16 +1283,6 @@ public class OPTICSManager extends CoordinateStore
 		// Use the same structure as OPTICS (since the algorithm is very similar)
 		initialiseOPTICS(generatingDistanceE, minPts);
 
-		// *************
-		// Note:
-		// Since DBSCAN just does counting in a neighbourhood we could change the implementation
-		// to use a high resolution 2D histogram. This can be searched using a circular mask.
-		// Only the points in the border cells of the circular mask need to be have a distance 
-		// computation performed. If the resolution is high enough (relative to the generating 
-		// distance) then the distance computation could be omitted altogether for speed. 
-		// This may have very little effect on the final output clusters.
-		// *************
-
 		// The distance may be updated
 		generatingDistanceE = grid.generatingDistanceE;
 
@@ -1240,33 +1346,33 @@ public class OPTICSManager extends CoordinateStore
 
 	private boolean expandCluster(Molecule object, float e, int minPts, Counter counter, MoleculeQueue seeds)
 	{
-		findNeighbours(minPts, object, e);
+		grid.findNeighbours(minPts, object, e);
 		if (counter.increment())
 			return true;
 
 		object.markProcessed();
-		object.setNumberOfPoints(neighbours.size);
-		if (neighbours.size >= minPts)
+		object.setNumberOfPoints(grid.neighbours.size);
+		if (grid.neighbours.size >= minPts)
 		{
 			// New cluster
 			int clusterId = counter.nextClusterId();
 			object.setClusterOrigin(clusterId);
 
-			// Expand through the neighbours
+			// Expand through the grid.neighbours
 			seeds.clear();
-			update(seeds, neighbours, clusterId);
+			update(seeds, grid.neighbours, clusterId);
 
 			while (seeds.hasNext())
 			{
 				object = seeds.next();
-				findNeighbours(minPts, object, e);
+				grid.findNeighbours(minPts, object, e);
 				if (counter.increment())
 					return true;
 
 				object.markProcessed();
-				object.setNumberOfPoints(neighbours.size);
-				if (neighbours.size >= minPts)
-					update(seeds, neighbours, clusterId);
+				object.setNumberOfPoints(grid.neighbours.size);
+				if (grid.neighbours.size >= minPts)
+					update(seeds, grid.neighbours, clusterId);
 			}
 		}
 
