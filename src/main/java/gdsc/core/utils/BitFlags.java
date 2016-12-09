045bd3199fd87cb08206e8f26d4e39b716129e11
aherbert <a.herbert@sussex.ac.uk>
2016-12-09 14:39:37 +0000

    Simple class for using int bit flags

---
+++ src/main/java/gdsc/core/utils/BitFlags.java
@@ -0,0 +1,58 @@
+package gdsc.core.utils;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC ImageJ Plugins Software
+ * 
+ * Copyright (C) 2016 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Class for manipulating bit flags
+ * 
+ * @author Alex Herbert
+ */
+public class BitFlags
+{
+	/**
+	 * Check if all of the given bits are set in the flags
+	 * 
+	 * @param flags
+	 * @param bits
+	 * @return True if all are set
+	 */
+	public static boolean areSet(final int flags, final int bits)
+	{
+		return (flags & bits) == bits;
+	}
+
+	/**
+	 * Check if any of the given bits are set in the flags
+	 * 
+	 * @param flags
+	 * @param bits
+	 * @return True if any are set
+	 */
+	public static boolean anySet(final int flags, final int bits)
+	{
+		return (flags & bits) != 0;
+	}
+
+	/**
+	 * Check if any of the given bits are not set in the flags
+	 * 
+	 * @param flags
+	 * @param bits
+	 * @return True if any are not set
+	 */
+	public static boolean anyNotSet(final int flags, final int bits)
+	{
+		return !areSet(flags, bits);
+	}
+}
