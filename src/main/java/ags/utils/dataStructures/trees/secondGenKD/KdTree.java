bc3a67edf2bc34d832299b330a7093d9ffd92c1b
aherbert <a.herbert@sussex.ac.uk>
2017-01-06 13:02:35 +0000

    Fixed pointRegionDist in SqrEuclid2D

--- src/main/java/ags/utils/dataStructures/trees/secondGenKD/KdTree.java
+++ src/main/java/ags/utils/dataStructures/trees/secondGenKD/KdTree.java
@@ -643,8 +643,8 @@ public abstract class KdTree<T>
 
 		protected double pointRegionDist(double[] point, double[] min, double[] max)
 		{
-			double dx = (point[0] > max[0]) ? point[0] - max[0] : point[0] - min[0];
-			double dy = (point[1] > max[1]) ? point[1] - max[1] : point[1] - min[1];
+			double dx = (point[0] > max[0]) ? point[0] - max[0] : (point[0] < min[0]) ? point[0] - min[0] : 0;
+			double dy = (point[1] > max[1]) ? point[1] - max[1] : (point[1] < min[1]) ? point[1] - min[1] : 0;
 			return dx * dx + dy * dy;
 		}
 	}
