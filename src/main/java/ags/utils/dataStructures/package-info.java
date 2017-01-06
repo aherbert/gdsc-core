a457bc2d5185f7d523f74dedd2eeb63ed90b177e
aherbert <a.herbert@sussex.ac.uk>
2017-01-06 12:21:42 +0000

    Added fast KD tree package for KNN search

---
+++ src/main/java/ags/utils/dataStructures/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The code contained here has been taken from https://bitbucket.org/rednaxela/knn-benchmark.
+ * <p>
+ * The KNN benchmark project contains various implementations of Kd-trees for performing efficient K-Nearest Neighbour
+ * searches. There are many implementations available, this package contains one of the fastest in the standard benchmark tests.
+ * <p>
+ * The code is Copyright 2009 Rednaxela
+ */
+package ags.utils.dataStructures;
