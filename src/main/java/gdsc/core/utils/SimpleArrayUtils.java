0695b8b6da07187595b010c331dfe043d010cf32
aherbert <a.herbert@sussex.ac.uk>
2017-02-17 15:54:21 +0000

    Added a class for array utilities

---
+++ src/main/java/gdsc/core/utils/SimpleArrayUtils.java
@@ -0,0 +1,147 @@
+package gdsc.core.utils;
+
+import java.util.Arrays;
+
+import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.set.hash.TIntHashSet;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC ImageJ Plugins Software
+ * 
+ * Copyright (C) 2017 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Class for manipulating arrays
+ * 
+ * @author Alex Herbert
+ */
+public class SimpleArrayUtils
+{
+	/**
+	 * Merge the two sets into a single set using a hashset. The order is undefined. Input sets are unchanged.
+	 *
+	 * @param s1
+	 *            the first set
+	 * @param s2
+	 *            the second set
+	 * @return the merged set
+	 */
+	public static int[] merge(int[] s1, int[] s2)
+	{
+		TIntHashSet set = new TIntHashSet();
+		set.addAll(s1);
+		set.addAll(s2);
+		return set.toArray();
+	}
+
+	/**
+	 * Merge the two sets into a single set in ascending order. Both sets are destructively modified.
+	 * <p>
+	 * Note: This is rarely as fast as calling {@link #merge(int[], int[])} and then performing a sort.
+	 *
+	 * @param s1
+	 *            the first set
+	 * @param s2
+	 *            the second set
+	 * @return the merged set
+	 * @deprecated Use merge and sort instead
+	 */
+	@Deprecated
+	public static int[] sortMerge(int[] s1, int[] s2)
+	{
+		s1 = flatten(s1);
+		s2 = flatten(s2);
+		Arrays.sort(s2);
+
+		int i = 0;
+		int j = 0;
+		TIntArrayList list = new TIntArrayList(Math.max(s1.length, s2.length));
+		while (i < s1.length && j < s2.length)
+		{
+			if (s1[i] < s2[j])
+			{
+				// Advance s1
+				list.add(s1[i++]);
+				while (i < s1.length && s1[i] < s2[j])
+				{
+					list.add(s1[i++]);
+				}
+			}
+			else if (s1[i] > s2[j])
+			{
+				// Advance s2
+				list.add(s2[j++]);
+				while (j < s2.length && s2[j] < s1[i])
+				{
+					list.add(s2[j++]);
+				}
+			}
+			else
+			{
+				// Advance both
+				list.add(s1[i++]);
+				j++;
+				while (i < s1.length && j < s2.length && s1[i] == s2[j])
+				{
+					list.add(s1[i++]);
+					j++;
+				}
+			}
+		}
+
+		// Add the remaining data
+		if (i != s1.length)
+		{
+			list.ensureCapacity(s1.length - i + list.size());
+			while (i < s1.length)
+			{
+				list.add(s1[i++]);
+			}
+		}
+		else if (j != s2.length)
+		{
+			list.ensureCapacity(s2.length - j + list.size());
+			while (j < s2.length)
+			{
+				list.add(s2[j++]);
+			}
+		}
+
+		return list.toArray();
+	}
+
+	/**
+	 * Flatten the array into an ascending array of unique values. Input data are destructively modified.
+	 * <p>
+	 * Inputting null will return a zero sized array.
+	 *
+	 * @param s
+	 *            the array
+	 * @return the new array
+	 */
+	public static int[] flatten(int[] s)
+	{
+		if (s == null)
+			return new int[0];
+		if (s.length <= 1)
+			return s;
+		Arrays.sort(s);
+		int c = 0;
+		for (int i = 1; i < s.length; i++)
+		{
+			if (s[i] != s[c])
+			{
+				s[++c] = s[i];
+			}
+		}
+		return Arrays.copyOf(s, c + 1);
+	}
+}
---
+++ src/test/java/gdsc/core/utils/SimpleArrayUtilsTest.java
@@ -0,0 +1,214 @@
+package gdsc.core.utils;
+
+import java.util.Arrays;
+
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.Well19937c;
+import org.apache.commons.math3.util.MathArrays;
+import org.junit.Assert;
+import org.junit.Test;
+
+import gdsc.core.ij.Utils;
+import gdsc.core.test.BaseTimingTask;
+import gdsc.core.test.TimingService;
+import gnu.trove.set.hash.TIntHashSet;
+
+public class SimpleArrayUtilsTest
+{
+	RandomGenerator r = new Well19937c(30051977);
+	TIntHashSet set = new TIntHashSet();
+
+	@Test
+	public void canFlatten()
+	{
+		testFlatten(new int[0]);
+		testFlatten(new int[10]);
+		for (int i = 0; i < 10; i++)
+		{
+			testFlatten(next(1, 10));
+			testFlatten(next(10, 10));
+			testFlatten(next(100, 10));
+		}
+	}
+
+	private void testFlatten(int[] s1)
+	{
+		set.clear();
+		set.addAll(s1);
+		int[] e = set.toArray();
+		Arrays.sort(e);
+
+		int[] o = SimpleArrayUtils.flatten(s1);
+		//System.out.printf("%s =? %s\n", Arrays.toString(e), Arrays.toString(o));
+		Assert.assertArrayEquals(e, o);
+	}
+
+	@Test
+	public void canSortMerge()
+	{
+		testSortMerge(new int[0], new int[10]);
+		testSortMerge(new int[10], new int[10]);
+		testSortMerge(new int[10], next(10, 10));
+		for (int i = 0; i < 10; i++)
+		{
+			testSortMerge(next(1, 10), next(1, 10));
+			testSortMerge(next(10, 10), next(1, 10));
+			testSortMerge(next(100, 10), next(1, 10));
+			testSortMerge(next(1, 10), next(10, 10));
+			testSortMerge(next(10, 10), next(10, 10));
+			testSortMerge(next(100, 10), next(10, 10));
+			testSortMerge(next(1, 10), next(100, 10));
+			testSortMerge(next(10, 10), next(100, 10));
+			testSortMerge(next(100, 10), next(100, 10));
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private void testSortMerge(int[] s1, int[] s2)
+	{
+		set.clear();
+		set.addAll(s1);
+		set.addAll(s2);
+		int[] e = set.toArray();
+		Arrays.sort(e);
+
+		int[] o = SimpleArrayUtils.sortMerge(s1, s2);
+		//System.out.printf("%s =? %s\n", Arrays.toString(e), Arrays.toString(o));
+		Assert.assertArrayEquals(e, o);
+	}
+
+	private int[] next(int size, int max)
+	{
+		int[] a = new int[size];
+		for (int i = 0; i < size; i++)
+			a[i] = r.nextInt(max);
+		return a;
+	}
+
+	private abstract class MyTimingTask extends BaseTimingTask
+	{
+		int[][][] data;
+
+		public MyTimingTask(String name, int[][][] data)
+		{
+			super(name);
+			this.data = data;
+		}
+
+		public int getSize()
+		{
+			return data.length;
+		}
+
+		public Object getData(int i)
+		{
+			return new int[][] { data[i][0].clone(), data[i][1].clone() };
+		}
+	}
+
+	@Test
+	public void testOnIndexData()
+	{
+		double[] f = new double[] { 0.1, 0.5, 0.75 };
+		for (int size : new int[] { 100, 1000, 10000 })
+			for (int i = 0; i < f.length; i++)
+				for (int j = i; j < f.length; j++)
+				{
+					testOnIndexData(100, size, (int) (size * f[i]), (int) (size * f[j]));
+				}
+	}
+
+	public void testOnIndexData(int length, final int size, final int n1, final int n2)
+	{
+		int[][][] data = new int[length][2][];
+		int[] s1 = Utils.newArray(size, 0, 1);
+		for (int i = 0; i < length; i++)
+		{
+			MathArrays.shuffle(s1, r);
+			data[i][0] = Arrays.copyOf(s1, n1);
+			MathArrays.shuffle(s1, r);
+			data[i][1] = Arrays.copyOf(s1, n2);
+		}
+		String msg = String.format("[%d] %d vs %d", size, n1, n2);
+
+		TimingService ts = new TimingService();
+		ts.execute(new MyTimingTask("SortMerge" + msg, data)
+		{
+			@SuppressWarnings("deprecation")
+			public Object run(Object data)
+			{
+				int[][] d = (int[][]) data;
+				return SimpleArrayUtils.sortMerge(d[0], d[1]);
+			}
+		});
+		ts.execute(new MyTimingTask("merge+sort" + msg, data)
+		{
+			public Object run(Object data)
+			{
+				int[][] d = (int[][]) data;
+				int[] a = SimpleArrayUtils.merge(d[0], d[1]);
+				Arrays.sort(a);
+				return a;
+			}
+		});
+
+		ts.repeat(ts.getSize());
+		ts.report();
+	}
+
+	@Test
+	public void testOnRedundantData()
+	{
+		int[] n = new int[] { 2, 4, 8 };
+		int[] size = new int[] { 100, 1000, 10000 };
+
+		for (int i = 0; i < n.length; i++)
+			for (int j = i; j < n.length; j++)
+				for (int ii = 0; ii < size.length; ii++)
+					for (int jj = ii; jj < size.length; jj++)
+						testOnRedundantData(50, size[ii], n[i], size[jj], n[j]);
+	}
+
+	public void testOnRedundantData(int length, final int n1, final int r1, final int n2, final int r2)
+	{
+		int[][][] data = new int[length][2][];
+		int[] s1 = new int[n1];
+		for (int i = 0; i < n1; i++)
+			s1[i] = i % r1;
+		int[] s2 = new int[n2];
+		for (int i = 0; i < n2; i++)
+			s2[i] = i % r2;
+		for (int i = 0; i < length; i++)
+		{
+			MathArrays.shuffle(s1, r);
+			data[i][0] = s1.clone();
+			MathArrays.shuffle(s2, r);
+			data[i][1] = s2.clone();
+		}
+		String msg = String.format("%d%%%d vs %d%%%d", n1, r1, n2, r2);
+
+		TimingService ts = new TimingService();
+		ts.execute(new MyTimingTask("SortMerge" + msg, data)
+		{
+			@SuppressWarnings("deprecation")
+			public Object run(Object data)
+			{
+				int[][] d = (int[][]) data;
+				return SimpleArrayUtils.sortMerge(d[0], d[1]);
+			}
+		});
+		ts.execute(new MyTimingTask("merge+sort" + msg, data)
+		{
+			public Object run(Object data)
+			{
+				int[][] d = (int[][]) data;
+				int[] a = SimpleArrayUtils.merge(d[0], d[1]);
+				Arrays.sort(a);
+				return a;
+			}
+		});
+
+		ts.repeat(ts.getSize());
+		ts.report();
+	}
+}
