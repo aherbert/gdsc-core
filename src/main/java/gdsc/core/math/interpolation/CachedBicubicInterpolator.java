8b2668ec924becea1c98f075b2350a873374cdda
ah403 <ah403@gdsc018424.gdsc.susx.ac.uk>
2017-11-17 19:35:25 +0000

    More methods for initialisation and evaluation

--- src/main/java/gdsc/core/math/interpolation/CachedBicubicInterpolator.java
+++ src/main/java/gdsc/core/math/interpolation/CachedBicubicInterpolator.java
@@ -46,6 +46,39 @@ public class CachedBicubicInterpolator
 	}
 
 	/**
+	 * Update coefficients.
+	 * <p>
+	 * Note that if x=-1 and x=2 are not available then they can be replaced with x=1 and x=0. This is because the cubic
+	 * interpolation uses the points to construct the gradient at x=0 as ((x=1)-(x=-1)) / 2. Setting x=-1 to x=1 with
+	 * just zero the gradient at x=0. Likewise for the gradient at x=1 = ((x=2)-(x=0))/2. Similar arguments apply to y.
+	 *
+	 * @param p
+	 *            the value of the function at x=-1 to x=2 and y=-1 to y=2, packed in yx order.
+	 * @return the value
+	 */
+	public void updateCoefficients(double[] p)
+	{
+		//@formatter:off
+		a00 = p[5];
+		a01 = -.5*p[1] + .5*p[9];
+		a02 = p[1] - 2.5*p[5] + 2*p[9] - .5*p[13];
+		a03 = -.5*p[1] + 1.5*p[5] - 1.5*p[9] + .5*p[13];
+		a10 = -.5*p[4] + .5*p[6];
+		a11 = .25*p[0] - .25*p[8] - .25*p[2] + .25*p[10];
+		a12 = -.5*p[0] + 1.25*p[4] - p[8] + .25*p[12] + .5*p[2] - 1.25*p[6] + p[10] - .25*p[14];
+		a13 = .25*p[0] - .75*p[4] + .75*p[8] - .25*p[12] - .25*p[2] + .75*p[6] - .75*p[10] + .25*p[14];
+		a20 = p[4] - 2.5*p[5] + 2*p[6] - .5*p[7];
+		a21 = -.5*p[0] + .5*p[8] + 1.25*p[1] - 1.25*p[9] - p[2] + p[10] + .25*p[3] - .25*p[11];
+		a22 = p[0] - 2.5*p[4] + 2*p[8] - .5*p[12] - 2.5*p[1] + 6.25*p[5] - 5*p[9] + 1.25*p[13] + 2*p[2] - 5*p[6] + 4*p[10] - p[14] - .5*p[3] + 1.25*p[7] - p[11] + .25*p[15];
+		a23 = -.5*p[0] + 1.5*p[4] - 1.5*p[8] + .5*p[12] + 1.25*p[1] - 3.75*p[5] + 3.75*p[9] - 1.25*p[13] - p[2] + 3*p[6] - 3*p[10] + p[14] + .25*p[3] - .75*p[7] + .75*p[11] - .25*p[15];
+		a30 = -.5*p[4] + 1.5*p[5] - 1.5*p[6] + .5*p[7];
+		a31 = .25*p[0] - .25*p[8] - .75*p[1] + .75*p[9] + .75*p[2] - .75*p[10] - .25*p[3] + .25*p[11];
+		a32 = -.5*p[0] + 1.25*p[4] - p[8] + .25*p[12] + 1.5*p[1] - 3.75*p[5] + 3*p[9] - .75*p[13] - 1.5*p[2] + 3.75*p[6] - 3*p[10] + .75*p[14] + .5*p[3] - 1.25*p[7] + p[11] - .25*p[15];
+		a33 = .25*p[0] - .75*p[4] + .75*p[8] - .25*p[12] - .75*p[1] + 2.25*p[5] - 2.25*p[9] + .75*p[13] + .75*p[2] - 2.25*p[6] + 2.25*p[10] - .75*p[14] - .25*p[3] + .75*p[7] - .75*p[11] + .25*p[15];
+		//@formatter:on
+	}
+	
+	/**
 	 * Gets the interpolated value.
 	 *
 	 * @param x
@@ -61,6 +94,34 @@ public class CachedBicubicInterpolator
 		double y2 = y * y;
 		double y3 = y2 * y;
 
+		//@formatter:off
+		return (a00 + a01 * y + a02 * y2 + a03 * y3) +
+		       (a10 + a11 * y + a12 * y2 + a13 * y3) * x +
+		       (a20 + a21 * y + a22 * y2 + a23 * y3) * x2 +
+		       (a30 + a31 * y + a32 * y2 + a33 * y3) * x3;
+		//@formatter:on
+	}
+
+	/**
+	 * Gets the interpolated value. The power must be computed correctly. This function allows you to pre-compute the
+	 * powers for efficient sub-sampling.
+	 *
+	 * @param x
+	 *            the x (between 0 and 1)
+	 * @param x2
+	 *            x^2
+	 * @param x3
+	 *            x^3
+	 * @param y
+	 *            the y (between 0 and 1)
+	 * @param y2
+	 *            y^2
+	 * @param y3
+	 *            y^3
+	 * @return the value
+	 */
+	public double getValue(double x, double x2, double x3, double y, double y2, double y3)
+	{
 		//@formatter:off
 		return (a00 + a01 * y + a02 * y2 + a03 * y3) +
 		       (a10 + a11 * y + a12 * y2 + a13 * y3) * x +
