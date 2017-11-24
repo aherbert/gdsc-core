8b12265db61f5633a4c5500b9f3cf83357b2af40
aherbert <a.herbert@sussex.ac.uk>
2017-11-24 16:55:20 +0000

    Added algorithms

--- src/main/java/gdsc/core/utils/Digest.java
+++ src/main/java/gdsc/core/utils/Digest.java
@@ -26,6 +26,46 @@ import java.security.MessageDigest;
  */
 public class Digest
 {
+    /**
+     * The MD2 message digest algorithm defined in RFC 1319.
+     */
+    public static final String MD2 = "MD2";
+
+    /**
+     * The MD5 message digest algorithm defined in RFC 1321.
+     */
+    public static final String MD5 = "MD5";
+
+    /**
+     * The SHA-1 hash algorithm defined in the FIPS PUB 180-2.
+     */
+    public static final String SHA_1 = "SHA-1";
+
+    /**
+     * The SHA-224 hash algorithm defined in the FIPS PUB 180-3.
+     * <p>
+     * Present in Oracle Java 8.
+     * </p>
+     *
+     * @since 1.11
+     */
+    public static final String SHA_224 = "SHA-224";
+
+    /**
+     * The SHA-256 hash algorithm defined in the FIPS PUB 180-2.
+     */
+    public static final String SHA_256 = "SHA-256";
+
+    /**
+     * The SHA-384 hash algorithm defined in the FIPS PUB 180-2.
+     */
+    public static final String SHA_384 = "SHA-384";
+
+    /**
+     * The SHA-512 hash algorithm defined in the FIPS PUB 180-2.
+     */
+    public static final String SHA_512 = "SHA-512";
+    
 	/**
 	 * Gets the digest.
 	 *
@@ -33,7 +73,7 @@ public class Digest
 	 *            the algorithm
 	 * @return the digest
 	 * @throws IllegalArgumentException
-	 *             If teh algorithm is not recognised
+	 *             If the algorithm is not recognised
 	 */
 	public static MessageDigest getDigest(String algorithm) throws IllegalArgumentException
 	{
