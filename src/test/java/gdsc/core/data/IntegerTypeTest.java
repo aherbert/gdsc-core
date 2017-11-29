66a403f27ee9012c3a96482ffabf48023dbbedef
aherbert <a.herbert@sussex.ac.uk>
2017-11-29 13:07:29 +0000

    Auto generate all valid integer types

--- src/test/java/gdsc/core/data/IntegerTypeTest.java
+++ src/test/java/gdsc/core/data/IntegerTypeTest.java
@@ -1,10 +1,49 @@
 package gdsc.core.data;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class IntegerTypeTest
 {
+	@Test
+	public void canGenerateIntegerType()
+	{
+		Assume.assumeTrue(false);
+		StringBuilder sb = new StringBuilder();
+		for (int bitDepth = 1; bitDepth <= 64; bitDepth++)
+			add(sb, true, bitDepth);
+		for (int bitDepth = 1; bitDepth <= 63; bitDepth++)
+			add(sb, false, bitDepth);
+		System.out.println(sb.toString());
+	}
+
+	private void add(StringBuilder sb, boolean signed, int bitDepth)
+	{
+		if (signed)
+		{
+			sb.append("    /** A signed ").append(bitDepth).append("-bit integer */\n");
+			sb.append("    SIGNED_").append(bitDepth).append(" {\n");
+			sb.append("    @Override public String getName() { return \"Signed ").append(bitDepth)
+					.append("-bit integer\"; }\n");
+			sb.append("    @Override public long getMin() { return ").append(minSigned(bitDepth)).append("L; }\n");
+			sb.append("    @Override public long getMax() { return ").append(maxSigned(bitDepth)).append("L; }\n");
+			sb.append("    @Override public boolean isSigned() { return true; }\n");
+		}
+		else
+		{
+			sb.append("    /** An unsigned ").append(bitDepth).append("-bit integer */\n");
+			sb.append("    UNSIGNED_").append(bitDepth).append(" {\n");
+			sb.append("    @Override public String getName() { return \"Unsigned ").append(bitDepth)
+					.append("-bit integer\"; }\n");
+			sb.append("    @Override public long getMin() { return 0L; }\n");
+			sb.append("    @Override public long getMax() { return ").append(maxUnsigned(bitDepth)).append("L; }\n");
+			sb.append("    @Override public boolean isSigned() { return false; }\n");
+		}
+		sb.append("    @Override public int getBitDepth() { return ").append(bitDepth).append("; }\n");
+		sb.append("    },\n");
+	}
+
 	@Test
 	public void canProvideIntegerTypeData()
 	{
@@ -13,7 +52,7 @@ public class IntegerTypeTest
 			int bd = type.getBitDepth();
 			Assert.assertTrue(type.getName().contains(Integer.toString(bd) + "-bit"));
 			Assert.assertEquals(type, IntegerType.forOrdinal(type.ordinal()));
-			
+
 			if (type.isSigned())
 			{
 				// Signed
