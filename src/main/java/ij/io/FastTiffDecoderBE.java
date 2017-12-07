c456d5b36424db5fc11aa96efb1d1715d50a6a77
aherbert <a.herbert@sussex.ac.uk>
2017-12-07 17:08:20 +0000

    static methods to decode to int or short

--- src/main/java/ij/io/FastTiffDecoderBE.java
+++ src/main/java/ij/io/FastTiffDecoderBE.java
@@ -19,13 +19,13 @@ import java.io.IOException;
 /**
  * A big-endian FastTiffDecoder
  */
-public class FastTiffDecoderBE extends FastTiffDecoder 
+public class FastTiffDecoderBE extends FastTiffDecoder
 {
 	protected FastTiffDecoderBE(SeekableStream in, File file)
 	{
 		super(in, file);
 	}
-	
+
 	@Override
 	public boolean isLittleEndian()
 	{
@@ -37,18 +37,48 @@ public class FastTiffDecoderBE extends FastTiffDecoder
 	{
 		return ((b1 << 24) + (b2 << 16) + (b3 << 8) + b4);
 	}
-	
+
 	@Override
 	protected int getShort(int b1, int b2)
 	{
 		return ((b1 << 8) + b2);
 	}
-	
+
 	@Override
 	protected long readLong() throws IOException
 	{
 		return ((long) readInt() << 32) + ((long) readInt() & 0xffffffffL);
 	}
-	
-	
+
+	/**
+	 * Convert the bytes (range 0-255) to an int.
+	 *
+	 * @param b1
+	 *            byte 1
+	 * @param b2
+	 *            byte 2
+	 * @param b3
+	 *            byte 3
+	 * @param b4
+	 *            byte 4
+	 * @return the int
+	 */
+	public static int toInt(int b1, int b2, int b3, int b4)
+	{
+		return ((b1 << 24) + (b2 << 16) + (b3 << 8) + b4);
+	}
+
+	/**
+	 * Convert the bytes (range 0-255) to a short.
+	 *
+	 * @param b1
+	 *            byte 1
+	 * @param b2
+	 *            byte 2
+	 * @return the short
+	 */
+	public static int toShort(int b1, int b2)
+	{
+		return ((b1 << 8) + b2);
+	}
 }
--- src/main/java/ij/io/FastTiffDecoderLE.java
+++ src/main/java/ij/io/FastTiffDecoderLE.java
@@ -19,13 +19,13 @@ import java.io.IOException;
 /**
  * A little-endian FastTiffDecoder
  */
-public class FastTiffDecoderLE extends FastTiffDecoder 
+public class FastTiffDecoderLE extends FastTiffDecoder
 {
 	protected FastTiffDecoderLE(SeekableStream in, File file)
 	{
 		super(in, file);
 	}
-	
+
 	@Override
 	public boolean isLittleEndian()
 	{
@@ -37,16 +37,48 @@ public class FastTiffDecoderLE extends FastTiffDecoder
 	{
 		return ((b4 << 24) + (b3 << 16) + (b2 << 8) + (b1 << 0));
 	}
-	
+
 	@Override
 	protected int getShort(int b1, int b2)
 	{
 		return ((b2 << 8) + b1);
 	}
-	
+
 	@Override
 	protected long readLong() throws IOException
 	{
 		return ((long) readInt() & 0xffffffffL) + ((long) readInt() << 32);
+	}
+
+	/**
+	 * Convert the bytes (range 0-255) to an int.
+	 *
+	 * @param b1
+	 *            byte 1
+	 * @param b2
+	 *            byte 2
+	 * @param b3
+	 *            byte 3
+	 * @param b4
+	 *            byte 4
+	 * @return the int
+	 */
+	public static int toInt(int b1, int b2, int b3, int b4)
+	{
+		return ((b4 << 24) + (b3 << 16) + (b2 << 8) + (b1 << 0));
+	}
+
+	/**
+	 * Convert the bytes (range 0-255) to a short.
+	 *
+	 * @param b1
+	 *            byte 1
+	 * @param b2
+	 *            byte 2
+	 * @return the short
+	 */
+	public static int toShort(int b1, int b2)
+	{
+		return ((b2 << 8) + b1);
 	}
 }
