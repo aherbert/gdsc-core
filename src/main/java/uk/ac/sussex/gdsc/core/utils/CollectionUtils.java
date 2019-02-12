e251b7d8c0750d26edd76d7741a6f3883df394ea
aherbert <a.herbert@sussex.ac.uk>
2019-02-12 17:17:33 +0000

    Utility class for collections

---
+++ src/main/java/uk/ac/sussex/gdsc/core/utils/CollectionUtils.java
@@ -0,0 +1,214 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2018 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils;
+
+import java.util.Collection;
+
+/**
+ * Utility class for working with {@link Collection}.
+ */
+public final class CollectionUtils {
+
+  /** No public construction. */
+  private CollectionUtils() {}
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for any of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @return true if any of the elements are in the collection
+   */
+  public static <T> boolean containsAny(Collection<T> collection, T element1, T element2) {
+    return collection.contains(element1) || collection.contains(element2);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for any of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @return true if any of the elements are in the collection
+   */
+  public static <T> boolean containsAny(Collection<T> collection, T element1, T element2,
+      T element3) {
+    return collection.contains(element1) || collection.contains(element2)
+        || collection.contains(element3);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for any of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @param element4 the fourth element
+   * @return true if any of the elements are in the collection
+   */
+  public static <T> boolean containsAny(Collection<T> collection, T element1, T element2,
+      T element3, T element4) {
+    return collection.contains(element1) || collection.contains(element2)
+        || collection.contains(element3) || collection.contains(element4);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for any of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @param element4 the fourth element
+   * @param element5 the fifth element
+   * @return true if any of the elements are in the collection
+   */
+  public static <T> boolean containsAny(Collection<T> collection, T element1, T element2,
+      T element3, T element4, T element5) {
+    return collection.contains(element1) || collection.contains(element2)
+        || collection.contains(element3) || collection.contains(element4)
+        || collection.contains(element5);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for any of the elements.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param first the first element
+   * @param rest the rest
+   * @return true if any of the elements are in the collection
+   */
+  @SafeVarargs
+  public static <T> boolean containsAny(Collection<T> collection, T first, T... rest) {
+    if (collection.contains(first)) {
+      return true;
+    }
+    for (T element : rest) {
+      if (collection.contains(element)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for all of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @return true if all of the elements are in the collection
+   */
+  public static <T> boolean containsAll(Collection<T> collection, T element1, T element2) {
+    return collection.contains(element1) && collection.contains(element2);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for all of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @return true if all of the elements are in the collection
+   */
+  public static <T> boolean containsAll(Collection<T> collection, T element1, T element2,
+      T element3) {
+    return collection.contains(element1) && collection.contains(element2)
+        && collection.contains(element3);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for all of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @param element4 the fourth element
+   * @return true if all of the elements are in the collection
+   */
+  public static <T> boolean containsAll(Collection<T> collection, T element1, T element2,
+      T element3, T element4) {
+    return collection.contains(element1) && collection.contains(element2)
+        && collection.contains(element3) && collection.contains(element4);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for all of the arguments.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param element1 the first element
+   * @param element2 the second element
+   * @param element3 the third element
+   * @param element4 the fourth element
+   * @param element5 the fifth element
+   * @return true if all of the elements are in the collection
+   */
+  public static <T> boolean containsAll(Collection<T> collection, T element1, T element2,
+      T element3, T element4, T element5) {
+    return collection.contains(element1) && collection.contains(element2)
+        && collection.contains(element3) && collection.contains(element4)
+        && collection.contains(element5);
+  }
+
+  /**
+   * Check if the {@link Collection#contains(Object)} method is true for all of the elements.
+   *
+   * @param <T> the generic type
+   * @param collection the collection
+   * @param first the first element
+   * @param rest the rest of the elements
+   * @return true if all of the elements are in the collection
+   */
+  @SafeVarargs
+  public static <T> boolean containsAll(Collection<T> collection, T first, T... rest) {
+    if (collection.contains(first)) {
+      for (T element : rest) {
+        if (!collection.contains(element)) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+}
---
+++ src/test/java/uk/ac/sussex/gdsc/core/utils/CollectionUtilsTest.java
@@ -0,0 +1,97 @@
+package uk.ac.sussex.gdsc.core.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.EnumSet;
+
+@SuppressWarnings({"javadoc"})
+public class CollectionUtilsTest {
+
+  enum SimpleEnum {
+    A, B, C, D, E, F, G, H, I, J, K, L, M
+  }
+
+  // For smaller test code
+  private static final SimpleEnum A = SimpleEnum.A;
+  private static final SimpleEnum B = SimpleEnum.B;
+  private static final SimpleEnum C = SimpleEnum.C;
+  private static final SimpleEnum D = SimpleEnum.D;
+  private static final SimpleEnum E = SimpleEnum.E;
+  private static final SimpleEnum F = SimpleEnum.F;
+  private static final SimpleEnum G = SimpleEnum.G;
+
+  @Test
+  public void testContainsAny() {
+    // 2 args
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(A), A, B));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(B), A, B));
+    Assertions.assertFalse(CollectionUtils.containsAny(EnumSet.of(G), A, B));
+
+    // 3 args
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(A), A, B, C));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(B), A, B, C));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(C), A, B, C));
+    Assertions.assertFalse(CollectionUtils.containsAny(EnumSet.of(G), A, B, C));
+
+    // 4 args
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(A), A, B, C, D));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(B), A, B, C, D));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(C), A, B, C, D));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(D), A, B, C, D));
+    Assertions.assertFalse(CollectionUtils.containsAny(EnumSet.of(G), A, B, C, D));
+
+    // 5 args
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(A), A, B, C, D, E));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(B), A, B, C, D, E));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(C), A, B, C, D, E));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(D), A, B, C, D, E));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(E), A, B, C, D, E));
+    Assertions.assertFalse(CollectionUtils.containsAny(EnumSet.of(G), A, B, C, D, E));
+
+    // 6 args
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(A), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(B), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(C), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(D), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(E), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAny(EnumSet.of(F), A, B, C, D, E, F));
+    Assertions.assertFalse(CollectionUtils.containsAny(EnumSet.of(G), A, B, C, D, E, F));
+  }
+
+  @Test
+  public void testContainsAll() {
+    // 2 args
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A), A, B));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(B), A, B));
+    Assertions.assertTrue(CollectionUtils.containsAll(EnumSet.of(A, B), A, B));
+
+    // 3 args
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A), A, B, C));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B), A, B, C));
+    Assertions.assertTrue(CollectionUtils.containsAll(EnumSet.of(A, B, C), A, B, C));
+
+    // 4 args
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A), A, B, C, D));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B), A, B, C, D));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C), A, B, C, D));
+    Assertions.assertTrue(CollectionUtils.containsAll(EnumSet.of(A, B, C, D), A, B, C, D));
+
+    // 5 args
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A), A, B, C, D, E));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B), A, B, C, D, E));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C), A, B, C, D, E));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C, D), A, B, C, D, E));
+    Assertions.assertTrue(CollectionUtils.containsAll(EnumSet.of(A, B, C, D, E), A, B, C, D, E));
+
+    // 6 args
+    // @formatter:off
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A), A, B, C, D, E, F));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B), A, B, C, D, E, F));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C), A, B, C, D, E, F));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C, D), A, B, C, D, E, F));
+    Assertions.assertFalse(CollectionUtils.containsAll(EnumSet.of(A, B, C, D, E), A, B, C, D, E, F));
+    Assertions.assertTrue(CollectionUtils.containsAll(EnumSet.of(A, B, C, D, E, F), A, B, C, D, E, F));
+    // @formatter:on
+  }
+}
