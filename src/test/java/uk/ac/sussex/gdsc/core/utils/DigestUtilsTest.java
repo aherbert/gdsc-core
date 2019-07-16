a51def76bee7f5b0f3e57ee922531b488f5929d4
aherbert <a.herbert@sussex.ac.uk>
2019-07-16 14:43:14 +0100

    Use RadixStringSampler

--- src/test/java/uk/ac/sussex/gdsc/core/utils/DigestUtilsTest.java
+++ src/test/java/uk/ac/sussex/gdsc/core/utils/DigestUtilsTest.java
@@ -1,11 +1,10 @@
 package uk.ac.sussex.gdsc.core.utils;
 
+import uk.ac.sussex.gdsc.core.utils.rng.RadixStringSampler;
 import uk.ac.sussex.gdsc.test.junit5.RandomSeed;
 import uk.ac.sussex.gdsc.test.junit5.SeededTest;
 import uk.ac.sussex.gdsc.test.rng.RngUtils;
 
-import org.apache.commons.math3.exception.NotStrictlyPositiveException;
-import org.apache.commons.math3.exception.util.LocalizedFormats;
 import org.apache.commons.rng.UniformRandomProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,8 +21,10 @@ public class DigestUtilsTest {
 
     final byte[] testBytes = new byte[50];
 
+    final RadixStringSampler sampler = new RadixStringSampler(r, 50, 16);
+
     for (int i = 0; i < 10; i++) {
-      final String testString = nextHexString(r, 50);
+      final String testString = sampler.sample();
       Assertions.assertEquals(org.apache.commons.codec.digest.DigestUtils.md5Hex(testString),
           DigestUtils.md5Hex(testString));
       r.nextBytes(testBytes);
@@ -33,44 +34,6 @@ public class DigestUtilsTest {
           org.apache.commons.codec.digest.DigestUtils.md5Hex(new ByteArrayInputStream(testBytes)),
           DigestUtils.md5Hex(new ByteArrayInputStream(testBytes)));
     }
-  }
-
-  /**
-   * Generates a random string of hex characters of length len.
-   *
-   * <p>Adapted from org.apache.commons.math3.random.RandomDataGenerator.
-   *
-   * @param ran the random provider
-   * @param len the len
-   * @return the string
-   * @throws NotStrictlyPositiveException the not strictly positive exception
-   */
-  private static String nextHexString(UniformRandomProvider ran, int len)
-      throws NotStrictlyPositiveException {
-    if (len <= 0) {
-      throw new NotStrictlyPositiveException(LocalizedFormats.LENGTH, len);
-    }
-
-    // Initialize output buffer
-    final StringBuilder outBuffer = new StringBuilder();
-
-    // Get int(len/2)+1 random bytes
-    final byte[] randomBytes = new byte[(len / 2) + 1];
-    ran.nextBytes(randomBytes);
-
-    // Convert each byte to 2 hex digits
-    for (int i = 0; i < randomBytes.length; i++) {
-      final String hex = Integer.toHexString(randomBytes[i] & 0xFF);
-
-      // Make sure we add 2 hex digits for each byte
-      if (hex.length() == 1) {
-        outBuffer.append('0');
-        outBuffer.append(hex.charAt(0));
-      } else {
-        outBuffer.append(hex);
-      }
-    }
-    return outBuffer.toString().substring(0, len);
   }
 
   @Test
