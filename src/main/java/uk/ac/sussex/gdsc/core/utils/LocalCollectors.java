40bf5655ebba802a2714a40774aeeb2fe033239f
aherbert <a.herbert@sussex.ac.uk>
2020-03-09 18:03:46 +0000

    Add LocalCollectors class.

---
+++ src/main/java/uk/ac/sussex/gdsc/core/utils/LocalCollectors.java
@@ -0,0 +1,97 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * Provides implementations of the {@link Collector} interface.
+ */
+public final class LocalCollectors {
+  /** No public instances. */
+  private LocalCollectors() {}
+
+  /**
+   * Returns a {@code Collector} that accumulates the input elements into a new {@code LocalList}.
+   *
+   * @param <T> the type of the input elements
+   * @return a {@code Collector} which collects all the input elements into a {@code LocalList}, in
+   *         encounter order
+   */
+  public static <T> Collector<T, ?, LocalList<T>> toLocalList() {
+    return new LocalListCollector<>();
+  }
+
+  /**
+   * Implementation of a Collector that uses a LocalList.
+   *
+   * @param <T> the generic type
+   */
+  private static class LocalListCollector<T> implements Collector<T, LocalList<T>, LocalList<T>> {
+    private static final Set<Collector.Characteristics> CHARACTERISTICS =
+        Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+
+    @Override
+    public Supplier<LocalList<T>> supplier() {
+      return LocalList::new;
+    }
+
+    @Override
+    public BiConsumer<LocalList<T>, T> accumulator() {
+      return LocalList::add;
+    }
+
+    @Override
+    public BinaryOperator<LocalList<T>> combiner() {
+      return (a, b) -> {
+        // Use specialised method for combining two local lists
+        a.addAll(b);
+        return a;
+      };
+    }
+
+    @Override
+    public Function<LocalList<T>, LocalList<T>> finisher() {
+      // Nothing to do
+      return a -> a;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+      return CHARACTERISTICS;
+    }
+  }
+}
---
+++ src/test/java/uk/ac/sussex/gdsc/core/utils/LocalCollectorsTest.java
@@ -0,0 +1,71 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils;
+
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the {@link LocalCollectors}.
+ */
+@SuppressWarnings({"javadoc"})
+public class LocalCollectorsTest {
+
+  @Test
+  public void testToLocalList() {
+    final Collector<Integer, ?, LocalList<Integer>> c = LocalCollectors.toLocalList();
+
+    final LocalList<Integer> list = IntStream.range(0, 100).parallel().boxed().collect(c);
+    Assertions.assertEquals(100, list.size());
+    list.sort(Integer::compare);
+    for (int i = 0; i < list.size(); i++) {
+      Assertions.assertEquals(i, list.unsafeGet(i));
+    }
+
+    // The finisher is not used by the stream as it is an identity function.
+    assertFinisherIsIndentityFunction(c);
+  }
+
+  private static <A> void
+      assertFinisherIsIndentityFunction(Collector<Integer, A, LocalList<Integer>> c) {
+    Assertions.assertTrue(c.characteristics().contains(Collector.Characteristics.IDENTITY_FINISH));
+
+    final Function<A, LocalList<Integer>> f = c.finisher();
+    Assertions.assertNotNull(f);
+
+    // Test it is an identity function.
+    Assertions.assertNull(f.apply(null));
+
+    final A a = c.supplier().get();
+    Assertions.assertSame(a, f.apply(a));
+  }
+}
