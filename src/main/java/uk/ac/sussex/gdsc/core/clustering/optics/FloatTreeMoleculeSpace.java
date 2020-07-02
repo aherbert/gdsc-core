76c1adce8586a3cb38fb5b5336cb55338d920572
aherbert <a.herbert@sussex.ac.uk>
2020-07-02 16:09:48 +0100

    Update tree molecule space to support 3D as well as 2D.

--- src/main/java/uk/ac/sussex/gdsc/core/clustering/optics/FloatTreeMoleculeSpace.java
+++ src/main/java/uk/ac/sussex/gdsc/core/clustering/optics/FloatTreeMoleculeSpace.java
@@ -28,6 +28,8 @@
 
 package uk.ac.sussex.gdsc.core.clustering.optics;
 
+import java.util.function.IntFunction;
+import uk.ac.sussex.gdsc.core.trees.FloatDistanceFunction;
 import uk.ac.sussex.gdsc.core.trees.FloatDistanceFunctions;
 import uk.ac.sussex.gdsc.core.trees.KdTrees;
 import uk.ac.sussex.gdsc.core.trees.ObjFloatKdTree;
@@ -44,6 +46,9 @@ class FloatTreeMoleculeSpace extends MoleculeSpace {
 
   /** The KD tree implementation. */
   private ObjFloatKdTree<Molecule> tree;
+
+  /** The distance function. */
+  private FloatDistanceFunction distanceFunction;
 
   /**
    * Instantiates a new tree molecule space.
@@ -62,14 +67,36 @@ class FloatTreeMoleculeSpace extends MoleculeSpace {
     final float[] xcoord = opticsManager.getXData();
     final float[] ycoord = opticsManager.getYData();
 
+    IntFunction<Molecule> factory;
+    if (opticsManager.is3d()) {
+      final float[] zcoord = opticsManager.getZData();
+      tree = KdTrees.newObjFloatKdTree(3);
+      distanceFunction = FloatDistanceFunctions.SQUARED_EUCLIDEAN_3D;
+      factory = i -> {
+        final float x = xcoord[i];
+        final float y = ycoord[i];
+        final float z = zcoord[i];
+        final Molecule m = new Molecule3d(i, x, y, z);
+        setOfObjects[i] = m;
+        tree.addPoint(new float[] {x, y, z}, m);
+        return m;
+      };
+    } else {
+      tree = KdTrees.newObjFloatKdTree(2);
+      distanceFunction = FloatDistanceFunctions.SQUARED_EUCLIDEAN_2D;
+      factory = i -> {
+        final float x = xcoord[i];
+        final float y = ycoord[i];
+        final Molecule m = new Molecule(i, x, y);
+        setOfObjects[i] = m;
+        tree.addPoint(new float[] {x, y}, m);
+        return m;
+      };
+    }
+
     setOfObjects = new Molecule[xcoord.length];
-    tree = KdTrees.newObjFloatKdTree(2);
     for (int i = 0; i < xcoord.length; i++) {
-      final float x = xcoord[i];
-      final float y = ycoord[i];
-      final Molecule m = new Molecule(i, x, y);
-      setOfObjects[i] = m;
-      tree.addPoint(new float[] {x, y}, m);
+      setOfObjects[i] = factory.apply(i);
     }
 
     return setOfObjects;
@@ -78,8 +105,8 @@ class FloatTreeMoleculeSpace extends MoleculeSpace {
   @Override
   void findNeighbours(int minPts, Molecule object, float generatingDistanceE) {
     neighbours.clear();
-    tree.findNeighbours(new double[] {object.x, object.y}, generatingDistanceE,
-        FloatDistanceFunctions.SQUARED_EUCLIDEAN_2D, (molecule, distance) -> {
+    tree.findNeighbours(new double[] {object.x, object.y}, generatingDistanceE, distanceFunction,
+        (molecule, distance) -> {
           molecule.setD((float) distance);
           neighbours.add(molecule);
         });
