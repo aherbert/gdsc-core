be3701a06fda8af500e487f73698ebc40dca7f6e
aherbert <a.herbert@sussex.ac.uk>
2020-07-13 15:09:01 +0100

    Test OpticsCluster

---
+++ src/test/java/uk/ac/sussex/gdsc/core/clustering/optics/OpticsClusterTest.java
@@ -0,0 +1,77 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.clustering.optics;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"javadoc"})
+public class OpticsClusterTest {
+  @Test
+  public void testNoLevel() {
+    final int start = 13;
+    final int end = 42;
+    final int clusterId = 6;
+    final OpticsCluster c = new OpticsCluster(start, end, clusterId);
+    Assertions.assertEquals(start, c.start);
+    Assertions.assertEquals(end, c.end);
+    Assertions.assertEquals(clusterId, c.getClusterId());
+    Assertions.assertEquals(end - start + 1, c.length());
+    Assertions.assertEquals(end - start + 1, c.size());
+    Assertions.assertEquals(0, c.getLevel());
+    Assertions.assertEquals(0, c.getNumberOfChildren());
+    final String s = c.toString();
+    Assertions.assertTrue(s.contains(Integer.toString(start)));
+    Assertions.assertTrue(s.contains(Integer.toString(end)));
+    Assertions.assertTrue(s.contains(Integer.toString(clusterId)));
+  }
+
+  @Test
+  public void testChildren() {
+    final int start = 13;
+    final int end = 42;
+    final int clusterId = 3;
+    // children are created first and added to larger clusters
+    final OpticsCluster c = new OpticsCluster(start + 2, end - 2, clusterId);
+    Assertions.assertEquals(0, c.getLevel());
+    Assertions.assertEquals(0, c.getNumberOfChildren());
+    final OpticsCluster c2 = new OpticsCluster(start + 1, end - 1, clusterId + 1);
+    c2.addChildCluster(c);
+    Assertions.assertEquals(1, c2.getNumberOfChildren());
+    Assertions.assertEquals(1, c.getLevel());
+    Assertions.assertEquals(0, c2.getLevel());
+    final OpticsCluster c3 = new OpticsCluster(start, end, clusterId + 2);
+    c3.addChildCluster(c2);
+    Assertions.assertEquals(1, c3.getNumberOfChildren());
+    Assertions.assertEquals(1, c2.getNumberOfChildren());
+    Assertions.assertEquals(2, c.getLevel());
+    Assertions.assertEquals(1, c2.getLevel());
+    Assertions.assertEquals(0, c3.getLevel());
+  }
+}
---
+++ src/test/java/uk/ac/sussex/gdsc/core/clustering/optics/OpticsDbscanClusterTest.java
@@ -0,0 +1,56 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.clustering.optics;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"javadoc"})
+public class OpticsDbscanClusterTest {
+  @Test
+  public void testNoLevel() {
+    final int start = 13;
+    final int end = 42;
+    final int clusterId = 6;
+    final int size = 7;
+    final OpticsDbscanCluster c = new OpticsDbscanCluster(start, end, clusterId, size);
+    Assertions.assertEquals(start, c.start);
+    Assertions.assertEquals(end, c.end);
+    Assertions.assertEquals(clusterId, c.getClusterId());
+    Assertions.assertEquals(end - start + 1, c.length());
+    Assertions.assertEquals(size, c.size());
+    Assertions.assertEquals(0, c.getLevel());
+    Assertions.assertEquals(0, c.getNumberOfChildren());
+    final String s = c.toString();
+    Assertions.assertTrue(s.contains(Integer.toString(start)));
+    Assertions.assertTrue(s.contains(Integer.toString(end)));
+    Assertions.assertTrue(s.contains(Integer.toString(clusterId)));
+    Assertions.assertTrue(s.contains(Integer.toString(size)));
+  }
+}
