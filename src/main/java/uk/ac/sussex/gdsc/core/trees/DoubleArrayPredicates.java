d40ae635aafc4454d921f4dca8c0213647579863
aherbert <a.herbert@sussex.ac.uk>
2020-07-15 15:13:54 +0100

    Add predicates for testing array equality

---
+++ src/main/java/uk/ac/sussex/gdsc/core/trees/DoubleArrayPredicates.java
@@ -0,0 +1,90 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.trees;
+
+import java.util.function.BiPredicate;
+
+/**
+ * Provide implementations for equality computation for {@code double[]} arrays.
+ */
+public enum DoubleArrayPredicates implements BiPredicate<double[], double[]> {
+  /**
+   * Compute array equality in N-dimensions using the {@code ==} operator, thus {@code -0.0} is
+   * equal to {@code 0.0}.
+   */
+  EQUALS_ND {
+    @Override
+    public boolean test(double[] p1, double[] p2) {
+      for (int i = 0; i < p1.length; i++) {
+        if (p1[i] == p2[i]) {
+          continue;
+        }
+        return false;
+      }
+      return true;
+    }
+  },
+  /**
+   * Compute array equality in 3-dimensions using the {@code ==} operator, thus {@code -0.0} is
+   * equal to {@code 0.0}.
+   */
+  EQUALS_3D {
+    @Override
+    public boolean test(double[] p1, double[] p2) {
+      return p1[0] == p2[0] && p1[1] == p2[1] && p1[2] == p2[2];
+    }
+  },
+  /**
+   * Compute array equality in 2-dimensions using the {@code ==} operator, thus {@code -0.0} is
+   * equal to {@code 0.0}.
+   */
+  EQUALS_2D {
+    @Override
+    public boolean test(double[] p1, double[] p2) {
+      return p1[0] == p2[0] && p1[1] == p2[1];
+    }
+  };
+
+  /**
+   * Return a predicate to compute array equality using the {@code ==} operator, thus {@code -0.0}
+   * is equal to {@code 0.0}.
+   *
+   * @param dimensions the dimensions
+   * @return the distance function
+   */
+  public static BiPredicate<double[], double[]> equals(int dimensions) {
+    if (dimensions == 2) {
+      return EQUALS_2D;
+    }
+    if (dimensions == 3) {
+      return EQUALS_3D;
+    }
+    return EQUALS_ND;
+  }
+}
---
+++ src/test/java/uk/ac/sussex/gdsc/core/trees/FloatArrayPredicatesTest.java
@@ -0,0 +1,89 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.trees;
+
+import java.util.function.BiPredicate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"javadoc"})
+public class FloatArrayPredicatesTest {
+  @Test
+  public void testEquals() {
+    final BiPredicate<float[], float[]> twod = FloatArrayPredicates.EQUALS_2D;
+    final BiPredicate<float[], float[]> threed = FloatArrayPredicates.EQUALS_3D;
+    final BiPredicate<float[], float[]> nd = FloatArrayPredicates.EQUALS_ND;
+    final float x = -100;
+    final float[] p1 = {0, 1, 2};
+    final float[] p2 = {x, 1, 2};
+    final float[] p3 = {0, x, 2};
+    final float[] p4 = {0, 1, x};
+    final float[] p5 = {-0f, 1, 2};
+    Assertions.assertTrue(twod.test(p1, p1));
+    Assertions.assertFalse(twod.test(p1, p2));
+    Assertions.assertFalse(twod.test(p1, p3));
+    Assertions.assertTrue(twod.test(p1, p4));
+    Assertions.assertTrue(twod.test(p1, p5));
+    Assertions.assertTrue(twod.test(p1, p1));
+    Assertions.assertFalse(twod.test(p2, p1));
+    Assertions.assertFalse(twod.test(p3, p1));
+    Assertions.assertTrue(twod.test(p4, p1));
+    Assertions.assertTrue(twod.test(p5, p1));
+
+    Assertions.assertTrue(threed.test(p1, p1));
+    Assertions.assertFalse(threed.test(p1, p2));
+    Assertions.assertFalse(threed.test(p1, p3));
+    Assertions.assertFalse(threed.test(p1, p4));
+    Assertions.assertTrue(threed.test(p1, p5));
+    Assertions.assertTrue(threed.test(p1, p1));
+    Assertions.assertFalse(threed.test(p2, p1));
+    Assertions.assertFalse(threed.test(p3, p1));
+    Assertions.assertFalse(threed.test(p4, p1));
+    Assertions.assertTrue(threed.test(p5, p1));
+
+    Assertions.assertTrue(nd.test(p1, p1));
+    Assertions.assertFalse(nd.test(p1, p2));
+    Assertions.assertFalse(nd.test(p1, p3));
+    Assertions.assertFalse(nd.test(p1, p4));
+    Assertions.assertTrue(nd.test(p1, p5));
+    Assertions.assertTrue(nd.test(p1, p1));
+    Assertions.assertFalse(nd.test(p2, p1));
+    Assertions.assertFalse(nd.test(p3, p1));
+    Assertions.assertFalse(nd.test(p4, p1));
+    Assertions.assertTrue(nd.test(p5, p1));
+  }
+
+  @Test
+  public void testCreateSquareEuclideanDistanceFunction() {
+    Assertions.assertSame(FloatArrayPredicates.EQUALS_ND, FloatArrayPredicates.equals(1));
+    Assertions.assertSame(FloatArrayPredicates.EQUALS_2D, FloatArrayPredicates.equals(2));
+    Assertions.assertSame(FloatArrayPredicates.EQUALS_3D, FloatArrayPredicates.equals(3));
+    Assertions.assertSame(FloatArrayPredicates.EQUALS_ND, FloatArrayPredicates.equals(4));
+  }
+}
