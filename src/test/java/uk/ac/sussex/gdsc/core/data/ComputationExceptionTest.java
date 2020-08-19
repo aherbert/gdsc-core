85e67ec185bcac4c567a37c3c6fd1a4e9f6fb4d7
aherbert <a.herbert@sussex.ac.uk>
2020-08-19 17:25:53 +0100

    Add ComputationExceptionTest

---
+++ src/test/java/uk/ac/sussex/gdsc/core/data/ComputationExceptionTest.java
@@ -0,0 +1,46 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2020 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.data;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"javadoc"})
+public class ComputationExceptionTest {
+  @Test
+  public void canConstruct() {
+    final String message = "Something";
+    final Throwable cause = new ComputationException();
+    Assertions.assertEquals(message, new ComputationException(message).getMessage());
+    Assertions.assertEquals(cause, new ComputationException(cause).getCause());
+    ComputationException ex = new ComputationException(message, cause);
+    Assertions.assertEquals(message, ex.getMessage());
+    Assertions.assertEquals(cause, ex.getCause());
+  }
+}
