855359c6753172032f915403db701f122b93a058
aherbert <a.herbert@sussex.ac.uk>
2022-02-25 13:22:04 +0000

    Add class to create a hash collection optimised for indices

---
+++ gdsc-core/src/main/java/uk/ac/sussex/gdsc/core/utils/HashIndexCollections.java
@@ -0,0 +1,242 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2022 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils;
+
+import com.koloboke.collect.map.hash.HashIntByteMapFactory;
+import com.koloboke.collect.map.hash.HashIntByteMaps;
+import com.koloboke.collect.map.hash.HashIntCharMapFactory;
+import com.koloboke.collect.map.hash.HashIntCharMaps;
+import com.koloboke.collect.map.hash.HashIntDoubleMapFactory;
+import com.koloboke.collect.map.hash.HashIntDoubleMaps;
+import com.koloboke.collect.map.hash.HashIntFloatMapFactory;
+import com.koloboke.collect.map.hash.HashIntFloatMaps;
+import com.koloboke.collect.map.hash.HashIntIntMapFactory;
+import com.koloboke.collect.map.hash.HashIntIntMaps;
+import com.koloboke.collect.map.hash.HashIntLongMapFactory;
+import com.koloboke.collect.map.hash.HashIntLongMaps;
+import com.koloboke.collect.map.hash.HashIntObjMapFactory;
+import com.koloboke.collect.map.hash.HashIntObjMaps;
+import com.koloboke.collect.map.hash.HashIntShortMapFactory;
+import com.koloboke.collect.map.hash.HashIntShortMaps;
+import com.koloboke.collect.set.hash.HashIntSetFactory;
+import com.koloboke.collect.set.hash.HashIntSets;
+
+/**
+ * Provides factories for the hash collections in the <a
+ * href="https://github.com/leventov/Koloboke">Koloboke</a> collections library with a keys domain
+ * optimised to store only index values. A valid index is any positive integer, including zero.
+ *
+ * <p>Note: This class exists as the Koloboke default implementation uses zero to store a not
+ * present key value. If zero is entered into the table then the not present key must be changed
+ * which incurs a performance overhead. The collection can be initialised with a keys domain for
+ * positive integers and a suitable not present key that will avoid the rebuilding overhead.
+ *
+ * <p>The factories returned by this class are initialised with a key domain of [0,
+ * Integer.MAX_VALUE]. The following are equivalent:
+ *
+ * <pre>
+ * HashIntIntMap m1 = HashIndexCollections.getHashIntIntMapFactory().newUpdatableMap();
+ * HashIntIntMap m2 =
+ *     HashIntIntMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE).newUpdatableMap();
+ * </pre>
+ *
+ * <h2>Note</h2>
+ *
+ * <p>The keys domain is a performance hint. It is not expected that the returned maps will prevent
+ * insertion of negative valued keys. The class is tested to ensure that the default factory
+ * collections in the dependency tree of the GDSC core library allow negative key insertion. However
+ * the factory is loaded from the classpath using a service loader. If the default classpath
+ * contains an implementation that validates the key domain then the returned map may exhibit this
+ * behaviour.
+ */
+public final class HashIndexCollections {
+
+  /** Lazy load the HashIntSetFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntSetFactoryHolder {
+    /** Factory. */
+    static final HashIntSetFactory FACTORY =
+        HashIntSets.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntByteMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntByteMapFactoryHolder {
+    /** Factory. */
+    static final HashIntByteMapFactory FACTORY =
+        HashIntByteMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntCharMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntCharMapFactoryHolder {
+    /** Factory. */
+    static final HashIntCharMapFactory FACTORY =
+        HashIntCharMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntDoubleMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntDoubleMapFactoryHolder {
+    /** Factory. */
+    static final HashIntDoubleMapFactory FACTORY =
+        HashIntDoubleMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntFloatMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntFloatMapFactoryHolder {
+    /** Factory. */
+    static final HashIntFloatMapFactory FACTORY =
+        HashIntFloatMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntIntMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntIntMapFactoryHolder {
+    /** Factory. */
+    static final HashIntIntMapFactory FACTORY =
+        HashIntIntMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntLongMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntLongMapFactoryHolder {
+    /** Factory. */
+    static final HashIntLongMapFactory FACTORY =
+        HashIntLongMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /** Lazy load the HashIntIntMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntObjMapFactoryHolder {
+    /** Factory. */
+    @SuppressWarnings("rawtypes")
+    static final HashIntObjMapFactory FACTORY =
+        HashIntObjMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+
+    /**
+     * Get an instance.
+     *
+     * @param <V> the value type
+     * @return the factory
+     */
+    @SuppressWarnings({"unchecked"})
+    static <V> HashIntObjMapFactory<V> instance() {
+      return FACTORY;
+    }
+  }
+
+  /** Lazy load the HashIntShortMapFactory for the domain [0, Integer.MAX_VALUE]. */
+  static final class HashIntShortMapFactoryHolder {
+    /** Factory. */
+    static final HashIntShortMapFactory FACTORY =
+        HashIntShortMaps.getDefaultFactory().withKeysDomain(0, Integer.MAX_VALUE);
+  }
+
+  /**
+   * No public construction.
+   */
+  private HashIndexCollections() {}
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntSetFactory getHashIntSetFactory() {
+    return HashIntSetFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntByteMapFactory getHashIntByteMapFactory() {
+    return HashIntByteMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntCharMapFactory getHashIntCharMapFactory() {
+    return HashIntCharMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntDoubleMapFactory getHashIntDoubleMapFactory() {
+    return HashIntDoubleMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntFloatMapFactory getHashIntFloatMapFactory() {
+    return HashIntFloatMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntIntMapFactory getHashIntIntMapFactory() {
+    return HashIntIntMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntLongMapFactory getHashIntLongMapFactory() {
+    return HashIntLongMapFactoryHolder.FACTORY;
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @param <V> the most general value type of the maps that could be constructed by the returned
+   *        factory
+   * @return the factory
+   */
+  public static <V> HashIntObjMapFactory<V> getHashIntObjMapFactory() {
+    return HashIntObjMapFactoryHolder.instance();
+  }
+
+  /**
+   * Gets a factory optimised for positive integer keys. The keys domain is [0, Integer.MAX_VALUE].
+   *
+   * @return the factory
+   */
+  public static HashIntShortMapFactory getHashIntShortMapFactory() {
+    return HashIntShortMapFactoryHolder.FACTORY;
+  }
+}
---
+++ gdsc-core/src/test/java/uk/ac/sussex/gdsc/core/utils/HashIndexCollectionsTest.java
@@ -0,0 +1,290 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2022 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils;
+
+import com.koloboke.collect.map.hash.HashIntByteMap;
+import com.koloboke.collect.map.hash.HashIntByteMapFactory;
+import com.koloboke.collect.map.hash.HashIntByteMaps;
+import com.koloboke.collect.map.hash.HashIntCharMap;
+import com.koloboke.collect.map.hash.HashIntCharMapFactory;
+import com.koloboke.collect.map.hash.HashIntCharMaps;
+import com.koloboke.collect.map.hash.HashIntDoubleMap;
+import com.koloboke.collect.map.hash.HashIntDoubleMapFactory;
+import com.koloboke.collect.map.hash.HashIntDoubleMaps;
+import com.koloboke.collect.map.hash.HashIntFloatMap;
+import com.koloboke.collect.map.hash.HashIntFloatMapFactory;
+import com.koloboke.collect.map.hash.HashIntFloatMaps;
+import com.koloboke.collect.map.hash.HashIntIntMap;
+import com.koloboke.collect.map.hash.HashIntIntMapFactory;
+import com.koloboke.collect.map.hash.HashIntIntMaps;
+import com.koloboke.collect.map.hash.HashIntLongMap;
+import com.koloboke.collect.map.hash.HashIntLongMapFactory;
+import com.koloboke.collect.map.hash.HashIntLongMaps;
+import com.koloboke.collect.map.hash.HashIntObjMap;
+import com.koloboke.collect.map.hash.HashIntObjMapFactory;
+import com.koloboke.collect.map.hash.HashIntObjMaps;
+import com.koloboke.collect.map.hash.HashIntShortMap;
+import com.koloboke.collect.map.hash.HashIntShortMapFactory;
+import com.koloboke.collect.map.hash.HashIntShortMaps;
+import com.koloboke.collect.set.hash.HashIntSet;
+import com.koloboke.collect.set.hash.HashIntSetFactory;
+import com.koloboke.collect.set.hash.HashIntSets;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.junit.jupiter.api.Assertions;
+import uk.ac.sussex.gdsc.test.junit5.SeededTest;
+import uk.ac.sussex.gdsc.test.rng.RngUtils;
+import uk.ac.sussex.gdsc.test.utils.RandomSeed;
+
+@SuppressWarnings({"javadoc"})
+class HashIndexCollectionsTest {
+  /** Keys in the key domain. */
+  private static final int[] KEYS = {0, 42, 999, 2136846, Integer.MAX_VALUE};
+
+  /**
+   * Keys not in the key domain.
+   *
+   * <h2>Note</h2>
+   *
+   * <p>Koloboke v1.0.0 used the keys domain as a performance hint for table initialisation.
+   * Out-of-domain keys should still be allowed. Filtering the key domain is expensive. The tests
+   * add out-of-domain keys to the collections to check the keys domain is a performance hint only.
+   * If tests fail when upgrading the Koloboke implementation then all use of hash collections with
+   * the index domain should be checked for performance against using the default domain (all
+   * integer values).
+   */
+  private static final int[] BAD_KEYS = {-1, -56757, Integer.MIN_VALUE};
+
+  @SeededTest
+  void canGetHashIntSetFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntSetFactory f = HashIndexCollections.getHashIntSetFactory();
+    final HashIntSet actual = f.newUpdatableSet();
+    final HashIntSet expected = HashIntSets.newUpdatableSet();
+
+    for (final int k : KEYS) {
+      actual.add(k);
+      expected.add(k);
+    }
+    Assertions.assertEquals(expected, actual);
+    for (int i = 0; i < 10; i++) {
+      final int k = r.nextInt() >>> 1;
+      actual.add(k);
+      expected.add(k);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      actual.add(k);
+      expected.add(k);
+    }
+    Assertions.assertEquals(expected, actual);
+    for (int i = 0; i < 10; i++) {
+      final int k = r.nextInt() | Integer.MIN_VALUE;
+      actual.add(k);
+      expected.add(k);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntByteMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntByteMapFactory f = HashIndexCollections.getHashIntByteMapFactory();
+    final HashIntByteMap actual = f.newUpdatableMap();
+    final HashIntByteMap expected = HashIntByteMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final byte v = (byte) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final byte v = (byte) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntCharMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntCharMapFactory f = HashIndexCollections.getHashIntCharMapFactory();
+    final HashIntCharMap actual = f.newUpdatableMap();
+    final HashIntCharMap expected = HashIntCharMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final char v = (char) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final char v = (char) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntDoubleMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntDoubleMapFactory f = HashIndexCollections.getHashIntDoubleMapFactory();
+    final HashIntDoubleMap actual = f.newUpdatableMap();
+    final HashIntDoubleMap expected = HashIntDoubleMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final double v = r.nextDouble();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final double v = r.nextDouble();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntFloatMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntFloatMapFactory f = HashIndexCollections.getHashIntFloatMapFactory();
+    final HashIntFloatMap actual = f.newUpdatableMap();
+    final HashIntFloatMap expected = HashIntFloatMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final float v = r.nextFloat();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final float v = r.nextFloat();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntIntMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntIntMapFactory f = HashIndexCollections.getHashIntIntMapFactory();
+    final HashIntIntMap actual = f.newUpdatableMap();
+    final HashIntIntMap expected = HashIntIntMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final int v = r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final int v = r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntLongMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntLongMapFactory f = HashIndexCollections.getHashIntLongMapFactory();
+    final HashIntLongMap actual = f.newUpdatableMap();
+    final HashIntLongMap expected = HashIntLongMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final long v = r.nextLong();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final int v = r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntObjMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntObjMapFactory<Integer> f = HashIndexCollections.getHashIntObjMapFactory();
+    final HashIntObjMap<Integer> actual = f.newUpdatableMap();
+    final HashIntObjMap<Integer> expected = HashIntObjMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final int v = r.nextInt();
+      actual.put(k, new Integer(v));
+      expected.put(k, new Integer(v));
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final int v = r.nextInt();
+      actual.put(k, new Integer(v));
+      expected.put(k, new Integer(v));
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @SeededTest
+  void canGetHashIntShortMapFactory(RandomSeed seed) {
+    final UniformRandomProvider r = RngUtils.create(seed.get());
+    final HashIntShortMapFactory f = HashIndexCollections.getHashIntShortMapFactory();
+    final HashIntShortMap actual = f.newUpdatableMap();
+    final HashIntShortMap expected = HashIntShortMaps.newUpdatableMap();
+
+    for (final int k : KEYS) {
+      final short v = (short) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+
+    for (final int k : BAD_KEYS) {
+      final short v = (short) r.nextInt();
+      actual.put(k, v);
+      expected.put(k, v);
+    }
+    Assertions.assertEquals(expected, actual);
+  }
+}
