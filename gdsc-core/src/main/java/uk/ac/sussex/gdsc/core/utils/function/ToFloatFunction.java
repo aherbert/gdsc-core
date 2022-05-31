f1c8ef803f439e2bf8d8e3b81e6efd665a9f7f28
aherbert <a.herbert@sussex.ac.uk>
2022-05-31 11:14:55 +0100

    Add float specilisation of Function

---
+++ gdsc-core/src/main/java/uk/ac/sussex/gdsc/core/utils/function/ToFloatFunction.java
@@ -0,0 +1,53 @@
+/*-
+ * #%L
+ * Genome Damage and Stability Centre ImageJ Core Package
+ *
+ * Contains code used by:
+ *
+ * GDSC ImageJ Plugins - Microscopy image analysis
+ *
+ * GDSC SMLM ImageJ Plugins - Single molecule localisation microscopy (SMLM)
+ * %%
+ * Copyright (C) 2011 - 2022 Alex Herbert
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package uk.ac.sussex.gdsc.core.utils.function;
+
+/**
+ * Represents a function that produces a float-valued result. This is the {@code float}-producing
+ * primitive specialization for {@link java.util.function.Function}.
+ *
+ * <p>This is a functional interface whose functional method is {@link #applyAsFloat(Object)}.
+ *
+ * <p>This is based on {@link java.util.function.ToDoubleFunction ToDoubleFunction} for manipulation
+ * of {@code float} values.
+ *
+ * @param <T> the type of the input to the function
+ *
+ * @see java.util.function.Function
+ */
+@FunctionalInterface
+public interface ToFloatFunction<T> {
+  /**
+   * Applies this function to the given argument.
+   *
+   * @param value the function argument
+   * @return the function result
+   */
+  float applyAsFloat(T value);
+}
