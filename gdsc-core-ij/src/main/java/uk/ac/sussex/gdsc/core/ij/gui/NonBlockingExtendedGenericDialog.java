47a617a66cfb5a04b28ee7cf8e2697e9e2c3197a
aherbert <a.herbert@sussex.ac.uk>
2023-05-15 11:55:02 +0100

    Add functionality to close the dialog when an associated image closes

--- gdsc-core-ij/src/main/java/uk/ac/sussex/gdsc/core/ij/gui/NonBlockingExtendedGenericDialog.java
+++ gdsc-core-ij/src/main/java/uk/ac/sussex/gdsc/core/ij/gui/NonBlockingExtendedGenericDialog.java
@@ -29,17 +29,21 @@
 package uk.ac.sussex.gdsc.core.ij.gui;
 
 import ij.IJ;
+import ij.ImagePlus;
 import ij.WindowManager;
+import ij.gui.ImageWindow;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 /**
  * This is an extension of {@link uk.ac.sussex.gdsc.core.ij.gui.ExtendedGenericDialog} that is
  * non-model.
  *
- * <p>The implementation is a copy of {@link ij.gui.NonBlockingGenericDialog}.
+ * <p>The implementation is adapted from {@link ij.gui.NonBlockingGenericDialog}.
  */
 public class NonBlockingExtendedGenericDialog extends ExtendedGenericDialog {
   private static final long serialVersionUID = 8535959215385211516L;
@@ -48,14 +52,31 @@ public class NonBlockingExtendedGenericDialog extends ExtendedGenericDialog {
 
   private boolean closed;
 
+  /** The associated image. This dialog gets closed when the image is closed. */
+  private transient ImagePlus imp;
+  /** The window listener to check whether the associated window gets closed. */
+  private transient WindowListener wl;
+
   /**
-   * Instantiates a new non blocking extended generic dialog.
+   * Create an instance.
    *
    * @param title the title
    */
   public NonBlockingExtendedGenericDialog(String title) {
     super(title, null);
     setModal(false);
+  }
+
+  /**
+   * Sets the associated image. This dialog is brought to the front when the image is activated and
+   * will close when the image is closed.
+   *
+   * <p>Must be called before the dialog is displayed.
+   *
+   * @param imp the image
+   */
+  public void setImage(ImagePlus imp) {
+    this.imp = imp;
   }
 
   @Override
@@ -67,6 +88,33 @@ public class NonBlockingExtendedGenericDialog extends ExtendedGenericDialog {
     if (!IJ.macroRunning()) { // add to Window menu on event dispatch thread
       final NonBlockingExtendedGenericDialog thisDialog = this;
       EventQueue.invokeLater(() -> WindowManager.addWindow(thisDialog));
+    }
+    if (imp != null) {
+      final ImageWindow win = imp.getWindow();
+      // when the associated image closes, also close this dialog
+      if (win != null) {
+        wl = new WindowAdapter() {
+          @Override
+          public void windowClosed(WindowEvent e) {
+            // sets wasCanceled=true and does dispose()
+            NonBlockingExtendedGenericDialog.super.windowClosing(null);
+          }
+
+          /**
+           * Put the dialog into the foreground when the image we work on gets into the foreground.
+           * Note: This has been moved into the WindowAdaptor as it must respond to changes in the
+           * associated window.
+           */
+          @Override
+          public void windowActivated(WindowEvent e) {
+            if (e.getOppositeWindow() != NonBlockingExtendedGenericDialog.this) {
+              toFront();
+            }
+            WindowManager.setWindow(NonBlockingExtendedGenericDialog.this);
+          }
+        };
+        win.addWindowListener(wl);
+      }
     }
     synchronized (lock) {
       while (!closed) {
@@ -118,5 +166,11 @@ public class NonBlockingExtendedGenericDialog extends ExtendedGenericDialog {
   public void dispose() {
     super.dispose();
     WindowManager.removeWindow(this);
+    if (imp != null) {
+      final ImageWindow win = imp.getWindow();
+      if (win != null && wl != null) {
+        win.removeWindowListener(wl);
+      }
+    }
   }
 }
